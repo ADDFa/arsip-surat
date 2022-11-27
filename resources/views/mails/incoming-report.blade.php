98a2c2aa02b4bc963a5c22a1bd8cca8935445e28
ADDFa <fratamaadha18@gmail.com>
2022-11-27 20:25:08 +0700

    report html

--- resources/views/mails/incoming-report.blade.php
+++ resources/views/mails/incoming-report.blade.php
@@ -1,5 +1,88 @@
 @extends('layouts.main')
 
 @section('content')
-<h1>Hello world ngentut iko laporan surat masuk</h1>
+
+<h1>
+    Hello world ngentut iko laporan surat masuk
+    <p></p>
+</h1>
+
+<div role="main-content">
+    <div class="aksi">
+        <div class="dropdown">
+            <span>Show</span>
+
+            <a class="btn btn-secondary dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                aria-expanded="false">
+                Dropdown link
+            </a>
+
+            <span>Entries</span>
+
+            <ul class="dropdown-menu">
+                <li><a class="dropdown-item" href="#">Action</a></li>
+                <li><a class="dropdown-item" href="#">Another action</a></li>
+                <li><a class="dropdown-item" href="#">Something else here</a></li>
+            </ul>
+        </div>
+
+        <form class="search">
+            <input type="text">
+        </form>
+    </div>
+
+    <table>
+        <thead>
+            <tr>
+                <th></th>
+                <th></th>
+                <th></th>
+                <th></th>
+                <th></th>
+                <th></th>
+                <th></th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+        </tbody>
+    </table>
+
+    <div class="info">
+        <p>Showing 1 to 3 Of 3 Entries</p>
+
+        <div>
+            <button>Sebelumnya</button>
+            <button>1</button>
+            <button>Selanjutnya</button>
+        </div>
+    </div>
+</div>
+
 @endsection
