694247dbd9ea6b3a1651978288113d989a1253df
ADDFa <fratamaadha18@gmail.com>
2022-11-27 19:32:57 +0700

    about html

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -2,6 +2,46 @@
 
 @section('content')
 
-<h1>Hello world ngentut iko about</h1>
+<h1>
+    Hello world ngentut iko about
+    <p></p>
+</h1>
+
+<div role="main-content">
+    <h3></h3>
+    <p></p>
+
+    <table>
+        <thead>
+            <tr>
+                <th></th>
+                <th></th>
+            </tr>
+        </thead>
+
+        <tbody>
+            <tr>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+            </tr>
+        </tbody>
+    </table>
+</div>
 
 @endsection
