8b3e9124dd4ab7ec68a3d058870ecb7f81e4b64f
ADDFa <fratamaadha18@gmail.com>
2022-11-27 19:44:22 +0700

    user html

--- resources/views/users/user.blade.php
+++ resources/views/users/user.blade.php
@@ -2,6 +2,85 @@
 
 @section('content')
 
-<h1>Hello world ngentut iko data pengguna</h1>
+<h1>
+    Hello world ngentut iko data pengguna
+    <p></p>
+</h1>
+
+<a href=""></a>
+
+<div role="main-content">
+    <div class="aksi">
+        <div class="dropdown">
+            <span>Show</span>
+
+            <a class="btn btn-secondary dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                aria-expanded="false">
+                Dropdown link
+            </a>
+
+            <span>Entries</span>
+
+            <ul class="dropdown-menu">
+                <li><a class="dropdown-item" href="#">Action</a></li>
+                <li><a class="dropdown-item" href="#">Another action</a></li>
+                <li><a class="dropdown-item" href="#">Something else here</a></li>
+            </ul>
+        </div>
+    </div>
+
+    <table>
+        <thead>
+            <tr>
+                <th></th>
+                <th></th>
+                <th></th>
+                <th></th>
+                <th></th>
+                <th></th>
+                <th></th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+        </tbody>
+    </table>
+
+    <div class="info">
+        <p></p>
+
+        <div>
+            <button></button>
+            <button></button>
+            <button></button>
+        </div>
+    </div>
+</div>
 
 @endsection
