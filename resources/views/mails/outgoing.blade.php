0944769fb9b9b60657b2a4278a4a8fad7807174d
ADDFa <fratamaadha18@gmail.com>
2022-11-27 20:10:40 +0700

    surat keluar html

--- resources/views/mails/outgoing.blade.php
+++ resources/views/mails/outgoing.blade.php
@@ -1,5 +1,88 @@
 @extends('layouts.main')
 
 @section('content')
-<h1>Hello world ngentut iko surat keluar</h1>
+<h1>
+    Hello world ngentut iko surat keluar
+    <p></p>
+</h1>
+
+<div role="main-content">
+    <div class="aksi">
+        <div class="dropdown">
+            <span>Show</span>
+
+            <a class="btn btn-secondary dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                aria-expanded="false">
+                Dropdown link
+            </a>
+
+            <span>Entries</span>
+
+            <ul class="dropdown-menu">
+                <li><a class="dropdown-item" href="#">Action</a></li>
+                <li><a class="dropdown-item" href="#">Another action</a></li>
+                <li><a class="dropdown-item" href="#">Something else here</a></li>
+            </ul>
+        </div>
+
+        <form class="search">
+            <input type="text">
+        </form>
+    </div>
+
+    <table>
+        <thead>
+            <tr>
+                <th></th>
+                <th></th>
+                <th></th>
+                <th></th>
+                <th></th>
+                <th></th>
+                <th></th>
+                <th></th>
+            </tr>
+        </thead>
+
+        <tbody>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td>
+                    <a href="">Detail</a>
+                    <a href="">Ubah</a>
+
+                    <form action="">
+                        <button>Hapus</button>
+                    </form>
+                </td>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+        </tbody>
+    </table>
+</div>
 @endsection
