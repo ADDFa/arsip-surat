9b68e37f96ff9fd12ecf24487cdc03ad9ed91af5
Faridho Catur Pamungkas <faridhocatur@gmail.com>
2022-12-21 20:28:21 +0700

    menambahkan modal surat masuk

--- resources/views/mails/incoming.blade.php
+++ resources/views/mails/incoming.blade.php
@@ -43,7 +43,7 @@
                 <th>Nomor Surat</th>
                 <th>Sifat Surat</th>
                 <th>Pengirim</th>
-                <th>Disposisi</th>
+                <th class="text-center">Disposisi</th>
                 <th class="text-center">Aksi</th>
             </tr>
         </thead>
@@ -55,7 +55,11 @@
                 <td>{{ $mail->mail_number }}</td>
                 <td>{{ $mail->mail_nature }}</td>
                 <td>{{ $mail->sender }}</td>
-                <td></td>
+                <td class="text-center">
+                    <button type="button" class="btn btn-warning" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                    <i class="bi bi-send"></i>
+                    </button>
+                </td>
                 <td class="action">
                     <a href="" class="btn btn-success">
                         <i class="bi bi-info-circle"></i>
@@ -88,5 +92,47 @@
             <button>Selanjutnya</button>
         </div>
     </div>
+</div>
+
+<div class="modal fade disposition" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h2 class="modal-title  fs-6" id="exampleModalLabel">
+            <span>Disposisi</span>
+            <span>Disposisi ....</span>
+        </h2>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <form>
+            <div class="mb-3">
+            <label for="regardingMail" class="form-label">Perihal</label>
+            <input type="text" class="form-control" id="regardingMail" name="regardingMail" value="Surat Undangan">
+            </div>
+
+            <div class="mb-3">
+            <label for="fromUnit" class="form-label">Dari Bagian</label>
+            <input type="text" class="form-control" id="fromUnit" name="fromUnit" value="Sekretaris Kepala Sekolah">
+            </div>
+
+            <div class="mb-3">
+            <label for="dispositionDestination" class="form-label">Disposisi Kepada</label>
+            <input type="text" class="form-control" id="dispositionDestination"  name="dispositionDestination" value="Kepala Sekolah">
+            </div>
+
+            <div class="mb-3">
+            <label for="dispositionContent" class="form-label">Isi Disposisi</label>
+            <textarea class="form-control" id="dispositionContent" name="dispositionContent" rows="3">Mohon segera diperiksa dan diteruskan kepada Waka Kesiswaan
+            </textarea>
+            </div>
+        </form>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-primary" data-bs-dismiss="modal">Simpan</button>
+        <button type="button" class="btn btn-warning">Batalkan</button>
+      </div>
+    </div>
+  </div>
 </div>
 @endsection
